85b64912aae98d71d769ef9c2a5ba98f8eb72f69
Manas <manasmahajan1301photos1@gmail.com>
2023-05-06 12:48:30 +0530

    added support for responsive images

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -19,9 +19,32 @@ const ProductCard = forwardRef<HTMLInputElement, ProductCardProps>(
     return (
       <div>
         <div ref={ref} className={styles.productImageContainer}>
-          <img
-            src={`${imgUrl}?w=720&h=900&q=50&fm=webp`}
-          />
+          <picture>
+            <source
+              type="image/webp"
+              srcSet={`${imgUrl}?w=360&h=450&q=50&fm=webp 360w,
+                ${imgUrl}?w=720&h=900&q=50&fm=webp 720w,
+                ${imgUrl}?w=1440&h=1800&q=50&fm=webp 1440w,
+                ${imgUrl}?w=1600&h=2000&q=50&fm=webp 1600w`}
+              sizes="(max-width: 1600px) 100vw, 1600px"
+            />
+            <source
+              srcSet={`${imgUrl}?w=360&h=450&q=50 360w,
+                ${imgUrl}?w=720&h=900&q=50 720w,
+                ${imgUrl}?w=1440&h=1800&q=50 1440w,
+                ${imgUrl}?w=1600&h=2000&q=50 1600w`}
+              sizes="(max-width: 1600px) 100vw, 1600px"
+            />
+            <img
+              srcSet={`${imgUrl}?w=360&h=450&q=50 360w,
+                ${imgUrl}?w=720&h=900&q=50 720w,
+                ${imgUrl}?w=1440&h=1800&q=50 1440w,
+                ${imgUrl}?w=1600&h=2000&q=50 1600w`}
+              src={`${imgUrl}?w=1600&q=50`}
+              sizes="(max-width: 1600px) 100vw, 1600px"
+              alt="hero-image"
+            />
+          </picture>
         </div>
         <div className={styles.bottomSectionWrapper}>
           <div className={styles.nameAndPriceSection}>
